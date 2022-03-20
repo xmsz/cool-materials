fb23619ff76f86e7613e57fb6c8dee07e67062fc
xmsz <137819866@qq.com>
2022-03-20 14:43:19 +0800

    style

---
+++ scaffolds/CoolManageScaffold/src/components/PaginationPlus/index.tsx
@@ -0,0 +1,25 @@
+import { Pagination } from '@alifd/next';
+import { PaginationProps } from '@alifd/next/types/pagination';
+
+const Between = (paginationProps: PaginationProps) => {
+  return (
+    <div className="items-center justify-between sm:flex">
+      <Pagination
+        {...paginationProps}
+        pageSizeSelector="dropdown"
+        pageSizeList={[10, 20, 50]}
+        className="next-pagination-hide-pages"
+      />
+
+      <div className="mt-4 sm:mt-0">
+        <Pagination shape="arrow-only" showJump={false} {...paginationProps} />
+      </div>
+    </div>
+  );
+};
+
+const PaginationPlus = {
+  Between,
+};
+
+export default PaginationPlus;
