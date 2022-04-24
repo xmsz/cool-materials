741888a27b0688d7ffd9480646e646b3ff4ef151
xmsz <137819866@qq.com>
2022-04-24 11:50:13 +0800

    doc

--- scaffolds/CoolManageScaffold/src/libs/auth.ts
+++ scaffolds/CoolManageScaffold/src/libs/auth.ts
@@ -1,5 +1,7 @@
 import { storage } from '@uni/apis';
+import { getSearchParams } from 'ice';
 import showErrMessage from './showErrMessage';
+import axios from 'axios';
 
 const { getStorageSync, removeStorageSync, setStorageSync } = storage;
 
@@ -29,11 +31,15 @@ class Auth {
 
   async signIn() {
     if (this.token) return;
-    console.log('signIn');
 
-    // const { code } = await login({});
-    // const signInRes = await UserService.MiniProgramLogin({ jsCode: code });
-    // if (signInRes.token) this.setToken(signInRes.token);
+    // STEP: 从Params获取code
+    const code = encodeURIComponent(String(getSearchParams()['code']));
+
+    // STEP: 换取token
+    const res = await axios.post<{ data: { token: string } }>('/api/authorization', { code });
+
+    // STEP: 设置token
+    this.setToken(res.data.data.token);
   }
 
   signOut() {
@@ -44,3 +50,5 @@ class Auth {
 const auth = new Auth();
 
 export default auth;
+
+// https://short-url.cpshelp.cn/?code=VWDbb6hr481zT2qHXzFjdtKX6Ohc%2BQ6UbrxQcFEle4Lpqm5mMB%2BG68HTggYbGCoQ6My%2BFRw8Bsc9nDZ2Zv%2BwWdISP5LGx9xiTP0t7Ivt7LhtZeyf93OA8EtgKJC22MQTlKN4A9NKzHuRD0c%2Bm1WcWjZcN6DYW0H6nFFflbCyjtw%3D
