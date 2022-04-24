a01557134489c525ec54703d412ba62977bc2462
xmsz <137819866@qq.com>
2022-04-24 11:17:37 +0800

    feat

--- scaffolds/CoolManageScaffold/src/libs/auth.ts
+++ scaffolds/CoolManageScaffold/src/libs/auth.ts
@@ -1,23 +1,46 @@
-import { getStorageSync } from '@uni/storage';
+import { storage } from '@uni/apis';
+import showErrMessage from './showErrMessage';
+
+const { getStorageSync, removeStorageSync, setStorageSync } = storage;
 
 class Auth {
-  token: string;
+  token: string | null;
+  private storageKey = 'access_token';
 
-  getToken() {
-    return this.token;
+  constructor() {
+    try {
+      this.token = getStorageSync({
+        key: this.storageKey,
+      }).data;
+    } catch (err) {
+      showErrMessage(err);
+    }
   }
 
-  init({ defaultStorageKey }: { defaultStorageKey: string }) {
-    const { data: token } = getStorageSync({
-      key: defaultStorageKey,
-    });
-    this.token = token;
+  removeToken() {
+    this.token = null;
+    removeStorageSync({ key: this.storageKey });
+  }
+
+  setToken(data: string) {
+    this.token = data;
+    setStorageSync({ key: this.storageKey, data });
+  }
+
+  async signIn() {
+    if (this.token) return;
+    console.log('signIn');
+
+    // const { code } = await login({});
+    // const signInRes = await UserService.MiniProgramLogin({ jsCode: code });
+    // if (signInRes.token) this.setToken(signInRes.token);
+  }
+
+  signOut() {
+    this.removeToken();
   }
 }
 
 const auth = new Auth();
-auth.init({
-  defaultStorageKey: 'access_token',
-});
 
 export default auth;
