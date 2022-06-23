d34663a4e16abd0751b54684fbb62a39a0612d7c
xmsz <137819866@qq.com>
2022-06-23 18:45:57 +0800

    feat: 新包

--- scaffolds/CoolManageScaffold/src/libs/auth.ts
+++ scaffolds/CoolManageScaffold/src/libs/auth.ts
@@ -1,9 +1,7 @@
-import { storage } from '@uni/apis';
+import { getStorageSync, setStorageSync, removeStorageSync } from '@uni/storage';
 import { getSearchParams } from 'ice';
 import showErrMessage from './showErrMessage';
 import axios from 'axios';
-
-const { getStorageSync, removeStorageSync, setStorageSync } = storage;
 
 class Auth {
   token: string | null;
@@ -50,5 +48,3 @@ class Auth {
 const auth = new Auth();
 
 export default auth;
-
-// https://short-url.cpshelp.cn/?code=VWDbb6hr481zT2qHXzFjdtKX6Ohc%2BQ6UbrxQcFEle4Lpqm5mMB%2BG68HTggYbGCoQ6My%2BFRw8Bsc9nDZ2Zv%2BwWdISP5LGx9xiTP0t7Ivt7LhtZeyf93OA8EtgKJC22MQTlKN4A9NKzHuRD0c%2Bm1WcWjZcN6DYW0H6nFFflbCyjtw%3D
