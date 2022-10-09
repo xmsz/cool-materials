5d460399611819d014c32dbf100cb6f612872742
xmsz <137819866@qq.com>
2022-10-09 18:30:29 +0800

    feat

--- scaffolds/MobileScaffold/src/components/Auth/index.tsx
+++ scaffolds/MobileScaffold/src/components/Auth/index.tsx
@@ -1,10 +1,12 @@
 import { useMount } from 'ahooks';
 import { createElement, PropsWithChildren, useState } from 'rax';
-import showErrMessage from '@/libs/showErrMessage';
+import { getErrMessage } from '@/libs/showErrMessage';
 import { isWeChatMiniProgram } from '@uni/env';
 import { promisify } from '@/libs/promisify';
 import { AuthService } from '@/services/auth';
 import { token } from '@/libs/token';
+import { Dialog } from '@alifd/meet';
+import { history } from 'rax-app';
 
 async function login() {
   const result = {
@@ -23,6 +25,8 @@ async function login() {
     const res = await AuthService.Login(loginRes);
     token.value = res.token;
 
+    result.isLogin = true;
+
     return result;
   } else {
     console.log('网页暂不支持登录');
@@ -38,11 +42,24 @@ const Auth = ({ children }: PropsWithChildren<{}>) => {
     // STEP: 登录
     login()
       .then(({ isLogin }) => {
+        console.log(1, isLogin);
         setIsLogin(isLogin);
       })
-      .catch(showErrMessage)
+      .catch((err) => {
+        const errMsg = getErrMessage(err);
+        Dialog.show({
+          title: '登录失败',
+          content: errMsg,
+          okProps: {
+            text: '重新登录',
+          },
+          onOk: () => {
+            history?.replace('/list');
+          },
+        });
+      })
       .finally(() => {
-        setIsLogin(true);
+        // setIsLogin(true);
       });
   });
 
--- scaffolds/MobileScaffold/src/libs/showErrMessage.ts
+++ scaffolds/MobileScaffold/src/libs/showErrMessage.ts
@@ -1,6 +1,6 @@
 import { showToast } from '@uni/toast';
 
-const getErrMessage = (err: any) => err.response?.data?.message || err.message || err.errMsg || err;
+export const getErrMessage = (err: any) => err.response?.data?.message || err.message || err.errMsg || err;
 export default (err: any) => {
   const content = getErrMessage(err);
   showToast({ content, type: 'fail' });
