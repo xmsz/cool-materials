25024f97f488f405aba99d835167353024814f54
xmsz <137819866@qq.com>
2022-10-19 16:40:05 +0800

    fet

--- scaffolds/CoolManageScaffold/src/libs/showErrMessage.ts
+++ scaffolds/CoolManageScaffold/src/libs/showErrMessage.ts
@@ -1,7 +1,10 @@
 import { Message } from '@alifd/next';
 
+export const getErrMessage = (err) => {
+  return err.response?.data?.message || err.message || err.errMsg || err;
+};
 export default (err: any) => {
-  const errMsg = err.response?.data?.message || err.message || err.errMsg || err;
+  const errMsg = getErrMessage(err);
   Message.notice({ content: errMsg });
   return errMsg;
 };
